c25a4fc9c3f41033f63b619f402c43f3ccf9df24
imjoo0 <im_joo0@naver.com>
2023-02-01 11:26:07 +0900

    api change

--- LMSapp/views/manage.py
+++ LMSapp/views/manage.py
@@ -112,8 +112,7 @@ def request_task():
 def get_ban(id):
     if request.method == 'GET':
         target_ban = callapi.get_ban(id)
-        print(target_ban['register_no'])
-        students = callapi.get_students(target_ban['register_no'])
+        students = callapi.get_students(1219)
         print(students)
         return jsonify({
             'target_ban': target_ban['register_no'],
