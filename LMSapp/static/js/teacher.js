9fa1527cde825b578b2db59d4ccf8f8f133b397b
imjoo0 <im_joo0@naver.com>
2023-02-01 16:38:52 +0900

    o

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -30,6 +30,7 @@ function change_question_kind(str){
         $('#invisible_for_1').show();
         $('#question_box').show();
     }else if( str == "일반"||"none"){
+        console.log('되야혀')
         $('#invisible_for_1').hide();
         $('#invisible_for_2').hide();
         $('#question_box').show();
