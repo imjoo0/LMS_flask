8d27ff018d8710fb80b6d8de05115648bfd5eb0c
imjoo0 <im_joo0@naver.com>
2023-02-03 16:31:25 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -38,8 +38,7 @@ def home():
         my_questions = Question.query.filter(Question.teacher_id == session['user_registerno']).all()
         return render_template('teacher.html',user=teacher_info,my_bans=mybans_info,all_ban=all_ban_info,students=mystudents_info, questions=my_questions,my_task_category=category_set,all_task_category=all_task_category)
     elif request.method == 'POST':
-        task_ids = request.form.getlist('task_ids[]')
-        getlist('remove_list_give[]')
+        task_ids = request.form['task_ids']
         print(task_ids)
         return jsonify({'result':'ok'})
 # 테스트 계정 id : T1031 pw동일  
