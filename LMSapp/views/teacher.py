1efbded4c6ecaaae60e81b17f9c6d178b9f2dfa1
imjoo0 <im_joo0@naver.com>
2023-02-03 14:12:07 +0900

    fu

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -38,6 +38,7 @@ def home():
         my_questions = Question.query.filter(Question.teacher_id == session['user_registerno']).all()
         return render_template('teacher.html',user=teacher_info,my_bans=mybans_info,all_ban=all_ban_info,students=mystudents_info, questions=my_questions,my_task_category=category_set,all_task_category=all_task_category)
     elif request.method == 'POST':
+        print('jeje')
         task_ids = request.form['task_ids']
         print(task_ids)
         return jsonify({'result':'ok'})
