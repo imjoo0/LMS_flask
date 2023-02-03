326bb6ce9becb3b6d11f530a37f74fb229a66336
imjoo0 <im_joo0@naver.com>
2023-02-03 13:06:34 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -51,7 +51,6 @@ function get_not_done_task(category_id){
         url: "/teacher/"+category_id,
         data: {},
         success: function (response) {
-            console.log(response)
             let tcb = '#task_contents_box'+category_id
             if(response["task"] == '없음'){
                 let temp_task_contents_box = `
