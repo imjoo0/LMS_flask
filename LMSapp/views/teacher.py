fd89fa03c7de0d4150481f670c7d3ada1035594a
imjoo0 <im_joo0@naver.com>
2023-02-06 15:42:10 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -24,11 +24,11 @@ def home():
         all_ban_info = callapi.all_ban_info()
         all_task_category = TaskCategory.query.all()
 
-        my_tasks = TaskBan.query.filter(TaskBan.teacher_id==session['user_registerno']).all()[-1]
+        my_tasks = TaskBan.query.filter(TaskBan.teacher_id==session['user_registerno']).all()
         if len(my_tasks)!=0:
             tc = []
             for task in my_tasks:
-                t = Task.query.filter(Task.id==task.task_id).all()
+                t = Task.query.filter(Task.id==task.task_id).all()[-1]
                 print(t)
                 # 오늘의 업무만 저장 
                 if t.startdate.date() <= Today and Today <= t.deadline.date(): 
