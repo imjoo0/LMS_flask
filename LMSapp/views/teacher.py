58d13ba1c9560176f9afc82e5855bd625d76baf6
imjoo0 <im_joo0@naver.com>
2023-02-06 15:32:32 +0900

    ttt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -28,7 +28,8 @@ def home():
         if len(my_tasks)!=0:
             tc = []
             for task in my_tasks:
-                t = Task.query.filter(Task.id==task.task_id).all()[0]
+                t = Task.query.filter(Task.id==task.task_id).all()
+                print(t)
                 # 오늘의 업무만 저장 
                 if t.startdate.date() <= Today and Today <= t.deadline.date(): 
                     tc.append(t.contents)
