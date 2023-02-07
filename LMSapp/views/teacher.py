86c509e9f2ec5c97a06a86aefffabf0a4de5e66c
imjoo0 <im_joo0@naver.com>
2023-02-07 11:07:00 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -152,7 +152,8 @@ def consulting(id):
         my_students = callapi.get_students(id)
         consulting_list = []
         for student in my_students:
-            consultings = Consulting.query.filter((Consulting.student_id==student['register_no']) & (Consulting.done != 1) & (Consulting.startdate <= current_time) & ( current_time <= Consulting.deadline )).all()
+            consultings = Consulting.query.filter((Consulting.student_id==student['register_no']) & (Consulting.done != 1) ).all()
+            print(consultings)
             print(consultings)
             target_data = {}
             target_data['s_id'] = student['register_no']
