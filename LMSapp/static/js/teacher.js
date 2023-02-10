1009dfd2dd6ce24f5bfeef88de4a64393f680ccf
imjoo0 <im_joo0@naver.com>
2023-02-10 15:14:59 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -160,7 +160,7 @@ function get_target_consulting(student){
     return consulting_history(c_id)
 }
 function consulting_history(c_id){
-    consulting_missed = $('#missed'+String(c_id)).checked;
+    consulting_missed = $('#missed'+String(c_id));
     console.log(consulting_missed)
     if (consulting_missed = true){
         console.log('funck')
