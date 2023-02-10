bdacc2555b291c3ca240cd27a5e091b28d94d90e
imjoo0 <im_joo0@naver.com>
2023-02-10 15:13:42 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -161,6 +161,7 @@ function get_target_consulting(student){
 }
 function consulting_history(c_id){
     consulting_missed = $('#missed'+String(c_id)).checked;
+    console.log(consulting_missed)
     if (consulting_missed = true){
         console.log('funck')
         consulting_reason = "부재중"
