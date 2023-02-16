da5b933fc117351716f28c8e12087cd7c2e7ac91
imjoo0 <im_joo0@naver.com>
2023-02-16 16:05:09 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -281,7 +281,10 @@ def done_consulting(ban_id,is_missed):
         if(len(consulting_list)==0):
             return jsonify({'consulting_history': '없음'})
         else: 
-            consulting_list.sort(key = lambda x:(-x['consulting_num']))
+            if(consulting_list['kind'] == '완료상담'):
+                consulting_list.sort(key = lambda x:(-x['consulting_num']))
+            else:
+                consulting_list.sort(key = lambda x:(-x['kind']))
             return jsonify({'consulting_history': consulting_list})
             
   
