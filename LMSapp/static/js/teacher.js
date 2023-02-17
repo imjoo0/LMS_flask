f4f573730887ddbf1d19741ae630fefc47cedc41
imjoo0 <im_joo0@naver.com>
2023-02-17 09:29:32 +0900

    ttt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -146,8 +146,7 @@ function consulting_history_view(ban_regi){
         get_consulting(ban_regi)
     }
 }
-
-async function get_consulting(ban_regi){
+function consulting_view(ban_regi){
     ban_regi = Number(ban_regi)
     if(ban_regi == 0){
         $('#consulting_title').html('상담할 반을 선택해주세요 ')
@@ -156,6 +155,8 @@ async function get_consulting(ban_regi){
         $('#consulting_title').html('오늘의 상담')
         get_consulting(ban_regi)
     }
+}
+async function get_consulting(ban_regi){
     await $.ajax({
         type: "GET",
         url: "/teacher/mystudents/"+ban_regi,
