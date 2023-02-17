d71edcf5102d0b55f584438ff18b49f176d1e813
imjoo0 <im_joo0@naver.com>
2023-02-17 11:09:58 +0900

    ttt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -150,6 +150,7 @@ function consulting_view(ban_regi){
     ban_regi = Number(ban_regi)
     if(ban_regi == 0){
         $('#consulting_title').html('상담할 반을 선택해주세요 ')
+        $('#consulting_msg').html('상담할 반을 선택해주세요 ');
         $('#consulting_student_list').hide();
     }else{
         $('#consulting_title').html('오늘의 상담')
