396de6363341b21ea89fba51a2c69fe7089ec761
imjoo0 <im_joo0@naver.com>
2023-02-17 10:51:17 +0900

    ttt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -169,11 +169,8 @@ async function get_consulting(ban_regi){
                 `;
                 $('#consulting_msg').html(temp_consulting_contents_box);
             }else{
+                $('#consulting_msg').empty();
                 $('#consulting_student_list').show();
-                let temp_consulting_contents_box = `
-                <p> 오늘 해야 할 상담 목록 🎉</p>
-                `;
-                $('#consulting_msg').html(temp_consulting_contents_box);
                 $('#today_consulting_box').empty()
                 for(i=0;i<response["consulting_student_list"].length;i++){
                     let target = response["consulting_student_list"][i]
