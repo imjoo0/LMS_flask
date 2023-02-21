adbf0bd2bc4f10464715007eff25ef1c32c74185
imjoo0 <im_joo0@naver.com>
2023-02-21 14:17:10 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -112,10 +112,9 @@ def task_category(done_code):
                     target_task.append(task_data)
                 category_set = list(set(category_set))
         else:
+            print(my_tasks)        
             category_set = '없음'
             target_task = '없음'
-        print(category_set)
-        print(target_task)
         return jsonify({'task_category' : category_set,'target_task':target_task})
         
     elif request.method =='POST':
