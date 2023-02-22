dad76e0bc28892fb7ebd5ddd3e520275691e034a
imjoo0 <im_joo0@naver.com>
2023-02-22 17:07:25 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -327,7 +327,7 @@ def question(id):
         else:
             new_answer = Answer(content=answer_contents,title=answer_title,created_at=Today,reject_code=o_ban_id,question_id = id)  
             if target_question.category == 2:
-                if o_ban_id != 0 :
+                if o_ban_id != '0' :
                     print('여기가 찍히면 안됨')
                     new_switch_student = SwitchStudent(ban_id = target_question.ban_id,switch_ban_id=o_ban_id,teacher_id = target_question.teacher_id,student_id=target_question.student_id,created_at=Today)
                     db.session.add(new_switch_student)
@@ -335,7 +335,7 @@ def question(id):
                 else:
                     pass
             else:
-                if o_ban_id != 0 :
+                if o_ban_id != '0' :
                     new_out_student = OutStudent(ban_id = target_question.ban_id,teacher_id = target_question.teacher_id,student_id=target_question.student_id,created_at=Today)
                     db.session.add(new_out_student)
                     db.session.commit()
