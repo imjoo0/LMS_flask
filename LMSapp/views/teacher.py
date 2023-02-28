b0620479d7033b167b19a2b948b34f17b3fa1bcd
imjoo0 <im_joo0@naver.com>
2023-02-28 15:05:07 +0900

    t

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -421,7 +421,7 @@ def comment(id,is_coco):
         # target_question = Question.query.get_or_404(id)
         comment_contents = request.form['comment_contents']
         try:
-            new_comment = Comment(contents=comment_contents,user_id=session['user_registerno'],question_id=id,parent_id=is_coco,created_at=Today)
+            new_comment = Comment(contents=comment_contents,user_id=session['user_registerno'],question_id=id,created_at=Today)
             db.session.add(new_comment)
             db.session.commit()
             return jsonify({'result': '댓글 작성 완료'})
