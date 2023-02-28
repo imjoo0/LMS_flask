fb2dbf3041da3ffc829d8a9f7144359185daf822
imjoo0 <im_joo0@naver.com>
2023-02-28 12:54:04 +0900

    t

--- LMSapp/static/js/admin.js
+++ LMSapp/static/js/admin.js
@@ -539,7 +539,7 @@ function getTeacherInfo(t_id){
             
             $('.chart-total-text1').html(`퇴소 학생 수:${os}`)
             $('.chart-total-text2').html(`이반 학생 수:${ss}`)
-            $('.pie-chart1').css('background',`conic-gradient(#B9CDE5, #B9CDE5 ${tp}%, #D99694 ${ssp}%, #D99694 ${ssp}%, #2B2B2B ${osp}%, #2B2B2B);`)
+            $('.pie-chart1').style('background',`conic-gradient(#B9CDE5, #B9CDE5 ${tp}%, #D99694 ${ssp}%, #D99694 ${ssp}%, #2B2B2B ${osp}%, #2B2B2B);`)
 
             // 업무
             let total_todo = chart['total_todo']
