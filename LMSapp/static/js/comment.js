c194f3cfcb48216d079add21ef257592aeb3b0c7
imjoo0 <im_joo0@naver.com>
2023-03-02 11:14:44 +0900

    week3

---
+++ LMSapp/static/js/comment.js
@@ -0,0 +1,25 @@
+// 문의 댓글 기능 
+function post_comment(q_id,is_coco){
+    let comment_contents = ''
+    if(is_coco == 0 ){
+        comment_contents = $('#comment_contents').val()
+    }else{
+        console.log('여기만 찍혀야 되는데')
+        comment_contents = $(`#comment_contents${is_coco}`).val()
+    }
+    if((comment_contents.length == 0)){
+        alert('댓글 내용을 입력해주세요')
+    }
+    $.ajax({
+            type: "POST",
+			url:'/teacher/comment/'+q_id+'/'+is_coco,
+			// data: JSON.stringify(jsonData), // String -> json 형태로 변환
+            data: {
+                comment_contents:comment_contents,
+            },
+            success: function (response) {{
+				alert(response["result"])
+                get_comment(q_id,is_coco)
+			}}
+		})
+}
--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -132,31 +132,7 @@ async function get_answer(q_id){
    $('#questionlist').hide()
    $('#questiondetail').show()
 }
-// 문의 댓글 기능 
-function post_comment(q_id,is_coco){
-    let comment_contents = ''
-    if(is_coco == 0 ){
-        comment_contents = $('#comment_contents').val()
-    }else{
-        console.log('여기만 찍혀야 되는데')
-        comment_contents = $(`#comment_contents${is_coco}`).val()
-    }
-    if((comment_contents.length == 0)){
-        alert('댓글 내용을 입력해주세요')
-    }
-    $.ajax({
-            type: "POST",
-			url:'/teacher/comment/'+q_id+'/'+is_coco,
-			// data: JSON.stringify(jsonData), // String -> json 형태로 변환
-            data: {
-                comment_contents:comment_contents,
-            },
-            success: function (response) {{
-				alert(response["result"])
-                get_comment(q_id,is_coco)
-			}}
-		})
-}
+
 
 // 상담 수행 관련 함수
 function get_consulting_history(){
