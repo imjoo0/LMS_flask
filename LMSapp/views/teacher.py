4b53352716b7d51f7294fff4848fba1c5ac3eedd
imjoo0 <im_joo0@naver.com>
2023-03-03 14:00:22 +0900

    week3

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -128,7 +128,9 @@ def task(done_code):
         # done_code == 1 이면 완료한 업무 
         # done_code == 0 이면 오늘의 업무
         t = TaskBan.get_taskbaninfo(session['user_registerno'])
-        print(t)
+        print(t['contents'])
+        for ta in t:
+            print(ta)
         if(done_code == 1): 
             my_tasks = tb_query.filter((TaskBan.teacher_id==session['user_registerno']) & (TaskBan.done == done_code) & (TaskBan.created_at == Today)).all()
         else: 
