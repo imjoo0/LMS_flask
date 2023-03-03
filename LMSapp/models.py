238fbb9ddf7c83d8d3128f8cc754c2a662c7adf1
imjoo0 <im_joo0@naver.com>
2023-03-03 16:24:49 +0900

    week3

--- LMSapp/models.py
+++ LMSapp/models.py
@@ -129,7 +129,7 @@ class Task(Base):
     def get_taskbaninfo(cls,teacher,done_code):
         query = msession.query(cls.id, cls.contents, cls.category_id,cls.url,cls.startdate,cls.deadline,cls.cycle,cls.priority,TaskBan.id.label('taskban_id'), TaskBan.created_at.label('taskban_createdat'))
         query = query.join(TaskBan).options(joinedload(cls.taskban))
-        result = query.filter(TaskBan.teacher_id == teacher).all()
+        result = query.filter(and_(TaskBan.teacher_id == teacher,TaskBan.done==done_code)).all()
 
         data = []
         for row in result:
