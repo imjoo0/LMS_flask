61e18bf5dbd2ac1a98d39ddd8713432b86ef782c
imjoo0 <im_joo0@naver.com>
2023-03-06 09:48:13 +0900

    model

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -466,12 +466,11 @@ async function get_task(done_code){
                                 $('#task_ban_box_incomplete'+i).append(temp_task_ban_box);
                             }
                         }
-                        
-                        let temp_task_button = `
-                        <button onclick="get_update_done()">업무 완료 저장</button>
-                        `
-                        $('#today_task_box0').append(temp_task_button)
-                    }
+                    }              
+                    let temp_task_button = `
+                    <button onclick="get_update_done()">업무 완료 저장</button>
+                    `
+                    $('#today_task_box0').append(temp_task_button)
                 }
             }
             
