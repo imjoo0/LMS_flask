c09a68201b9b8ebd1a464f158637dcb012840e0c
imjoo0 <im_joo0@naver.com>
2023-03-07 16:52:44 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -315,7 +315,7 @@ def download_file(q_id):
     attachment = Attachments.query.filter_by(question_id=q_id).first()
     if attachment is None:
         return "File not found."
-    return send_file(BytesIO(attachment.data),as_attachment=True)
+    return send_file(BytesIO(attachment.data),as_attachment=True, mimetype=attachment.mime_type )
 
 def save_attachment(file, q_id):
     attachment = Attachments(
