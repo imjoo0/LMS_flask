01c32ec62cfcd530fe1361b30e2806e2bcb48d61
imjoo0 <im_joo0@naver.com>
2023-03-09 10:31:55 +0900

    tt

--- LMSapp/models.py
+++ LMSapp/models.py
@@ -176,7 +176,10 @@ class TaskBan(db.Model):
             for t in t_id:
                 category = Task.query.filter((Task.id==t) & (Task.startdate <= current_time) & ( current_time <= Task.deadline ) & (Task.cycle == today_yoil or Task.cycle == 0)).first().categories
                 if category != None:
-                    cate_data.append(category)
+                    data = {}
+                    data['id'] = category.id
+                    data['name'] = category.name
+                    cate_data.append(data)
             cate_data=list(set(cate_data))
         return cate_data
     
