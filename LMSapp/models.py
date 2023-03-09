2e939f769730c513ee8fd21cdd88d282b42b105d
imjoo0 <im_joo0@naver.com>
2023-03-09 13:34:56 +0900

    tt

--- LMSapp/models.py
+++ LMSapp/models.py
@@ -184,24 +184,9 @@ class TaskBan(db.Model):
         return result
     
     @classmethod
-    def get_task(cls,teacher_id,done,cid):
-        result = []
-        #  해야 하는 업무들 가져오기 (task_id가 중복되지 않도록)
-        if done == 1:
-            t_id = [value for (value,) in list(set(cls.query.filter(teacher_id == teacher_id , done == done, cls.created_at == Today).with_entities(cls.task_id).all()))]
-        else:
-            t_id = [value for (value,) in list(set(cls.query.filter(teacher_id == teacher_id , done == done).with_entities(cls.task_id).all()))]
-        if len(t_id)!=0:
-            for t in t_id:
-                task = Task.query.filter((Task.id==t) & (Task.category_id == cid) & (Task.startdate <= current_time) & ( current_time <= Task.deadline ) & (Task.cycle == today_yoil or Task.cycle == 0)).first()
-                if task != None:
-                    result.append(task)
-        return result
-    
-    @classmethod
     def get_ban(cls,teacher_id,task_id):
         #  해야 하는 업무들 가져오기 (task_id가 중복되지 않도록)
-        tb = cls.query.filter(cls.teacher_id == teacher_id ,cls.task_id == task_id, cls.done==0).with_entities(cls.id,cls.ban_id).all()
+        tb = cls.query.filter(cls.teacher_id == teacher_id ,cls.task_id == task_id).with_entities(cls.id,cls.ban_id).all()
         tb = [{'id':taskbanlist[0], 'ban':callapi.get_ban(taskbanlist[1])['ban_name']} for taskbanlist in tb]
         tb = json.dumps(tb)
         return tb
