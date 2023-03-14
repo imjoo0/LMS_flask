358da9047ba2a8b580d0dac3a550e15b0b22a388
imjoo0 <im_joo0@naver.com>
2023-03-14 09:39:37 +0900

    tt

--- LMSapp/views/teacher.py
+++ LMSapp/views/teacher.py
@@ -147,6 +147,8 @@ def task_category(done_code):
         else: 
             target_task = '없음'
         # return render_template('teacher.html',mt = mt)
+        print(target_cate)
+        print(target_task)
         return jsonify({'target_task':target_task,'target_cate':target_cate})
     elif request.method =='POST':
         # done_code = 완료한 task의 id
