f1be7ef1072a00c3d3060a953f1b385289ac944b
imjoo0 <im_joo0@naver.com>
2023-03-14 09:42:30 +0900

    tt

--- LMSapp/models.py
+++ LMSapp/models.py
@@ -185,6 +185,7 @@ class TaskBan(db.Model):
         else:
             t_id = [value for (value,) in list(set(cls.query.filter(cls.teacher_id == teacher_id , cls.done == done).with_entities(cls.task_id).all()))]
         if len(t_id)!=0:
+            print(t_id)
             for t in t_id:
                 task = Task.query.filter((Task.id==t) & (Task.startdate <= current_time) & ( current_time <= Task.deadline ) & (Task.cycle == today_yoil or Task.cycle == 0)).first()
                 if task != None:
