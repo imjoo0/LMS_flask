3fc8cad2dec231fc42a6f04c0b677b4ef0e624bb
imjoo0 <im_joo0@naver.com>
2023-03-16 16:07:54 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -41,7 +41,7 @@ async function request_consulting(){
         } else {
             $('#consulting_target_ban').show()
         }
-      }, 1000);
+      }, 100);
     await $.ajax({
         url: '/manage/request',
         type: 'GET',
