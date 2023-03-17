3af3170f22314aae6bab80372cbaddae424c0f06
imjoo0 <im_joo0@naver.com>
2023-03-17 10:36:41 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -78,16 +78,10 @@ async function request_consulting() {
 // 다중 선택 반 선택 취소
 function delete_selected_ban(value){
     // select 요소에서 선택된 option 엘리먼트들을 가져옴
-    var selectedOptions = $('#consulting_target_ban option:selected').map(function() {
-        return $(this).val();
-      }).get().filter(function(value, index, self) {
-        return self.indexOf(value) === index;
-      });
-    console.log(selectedOptions)
-    console.log(value)
-    var selectedOptions = $('#consulting_target_ban').val()
-    console.log(selectedOptions)
-    selectedOptions.remove(value)
+    $('#consulting_target_ban option[value="' + value + '"]').hide(); 
+    // var selectedOptions = $('#consulting_target_ban').val()
+    // console.log(selectedOptions)
+    // selectedOptions.remove(value)
 }
 
 
