d7e25a51f5c783eaecada0ed8b00c0cf0e0abc1b
imjoo0 <im_joo0@naver.com>
2023-03-21 18:10:19 +0900

    tt

--- callapi.py
+++ callapi.py
@@ -12,7 +12,6 @@ def json_default(value):
 
 def purple_info(id,url):
     result = requests.post(config.api + url, headers=headers, data=json.dumps({'data':{'id': id}}))
-    print(result)
     result = result.json()
     if(len(result) > 0):
         if(len(result) == 1):
