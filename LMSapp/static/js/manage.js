db66c6e1a172efa141d87c65c8ca1d3f245b934c
imjoo0 <im_joo0@naver.com>
2023-03-21 17:23:25 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -136,6 +136,7 @@ function show_selections(){
     }
     // 선택된 학생 정보 변경 
     $('#consulting_target_students').val(selectedStudentList)
+    console.log($('select[name="consulting_target_students[]"]').val())
 }
 function delete_selected_student(idx){
     selectedStudentList.splice(idx,1)
