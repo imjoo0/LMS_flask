3634ce6b8af4e03c86bccc740046a59fac0cb519
imjoo0 <im_joo0@naver.com>
2023-03-23 11:19:34 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -53,7 +53,7 @@ function souldata(){
                 <td class="col-3">${teacher_name}</td>
                 <td class="col-2">${switch_count}</td>
                 <td class="col-2">${out_count}</td>
-                <td class="col-1" data-bs-toggle="modal" data-bs-target="#so_question" onclick="request_consulting(${register_no})">✅</td>`;
+                <td class="col-1" data-bs-toggle="modal" data-bs-target="#bandetail" onclick="request_consulting(${register_no})">✅</td>`;
             }
             $('#static_data1').html(temp_html)
         }
