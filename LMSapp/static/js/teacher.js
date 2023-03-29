9cef326b785c0d29ec3ee0beed9943883bdae782
imjoo0 <im_joo0@naver.com>
2023-03-29 13:27:44 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -61,7 +61,7 @@ function get_data() {
                                 </div>
                             </div>
                         </div>
-                        <table class="table text-center" id="class_list" style="width:100%; margin-top:50%;">
+                        <table class="table text-center" id="class_list" style="width:100%; margin-top:10%;">
                             <tbody style="width:100%;">
                                 <tr class="row">
                                     <th class="col-12">${name} (${semester}학기)</th>
