5e09c3cd165aa6bf707fdca7e982c46d5454083d
imjoo0 <im_joo0@naver.com>
2023-03-30 14:16:54 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -211,7 +211,7 @@ function getBanlist(){
                     const onList = response['outstudent']['data'].filter(a => a.ban_id == ban_data.ban_id );
                     // const consultingList = consulting.filter(c => c.student_id === student.register_no);
                     if (onList.length > 0) {
-                        const onList = consultingList[0];
+                        const onList = onList[0];
                         acc.push({
                             'b_id':ban_data.ban_id ,
                             'name':ban_data.name ,
