76417f47e5a10f6ce59baa71c81f83d1c7c02ae0
imjoo0 <im_joo0@naver.com>
2023-03-31 14:37:37 +0900

    Tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -108,6 +108,10 @@ function get_data() {
             let consulting = response['all_consulting'].length > 0 ? response['all_consulting'].filter(consulting => consulting.done === 0) : 0;
             let consulting_t = response['all_consulting'].length;
             let consulting_done = consulting_t - consulting.length
+            console.log(typeof response['all_task'][0]['created_at'])
+            console.log(response['all_task'][0]['created_at'])
+            console.log(typeof today)
+            console.log(today)
             let task_done = response['all_task'].length > 0 ? response['all_task'].filter(task => task.done === 1 && task.created_at == today).length : 0;
             let task_t = response['all_task'].length > 0 ? response['all_task'].filter(task => task.done === 0).length : 0;
 
