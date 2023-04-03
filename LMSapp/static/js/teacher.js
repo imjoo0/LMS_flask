2245dd401e8c717b035b0526c76507daa0c5ebde
imjoo0 <im_joo0@naver.com>
2023-04-03 17:22:42 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -284,17 +284,16 @@ function get_data() {
                 });
                 $('#consulting_title').html('오늘의 상담');
                 consultingStudentData = result
-                console.log(result)
-                console.log(today)
-                console.log(result.filter(e=>e.missed == today))
                 container.pagination({
-                    dataSource: result.filter(e=>e.missed == today),
+                    dataSource: result.filter(e=>e.missed != today),
                     prevText: '이전',
                     nextText: '다음',
                     pageSize: 10,
                     callback: function (result, pagination) {
                         let temp_consulting_contents_box = ''
                         $.each(result, function (index, consulting) {
+                            console.log(consulting.missed)
+                            console.log(today)
                             temp_consulting_contents_box += `
                             <td class="col-3">${consulting.ban_name}</td>
                             <td class="col-2">${consulting.student_name}</td>
