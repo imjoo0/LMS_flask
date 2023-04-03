b9e175f7cd55d5ec0bcf5fcd02bef50cb4bad7fa
imjoo0 <im_joo0@naver.com>
2023-04-03 18:28:31 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -233,8 +233,13 @@ function get_data() {
                                 console.log(task_items)
                                 for(k=0; k < task_items.length; k++){
                                     const ban_name = response['ban_data'].filter(a => a.register_no === task_items[k].ban_id)[0]['name']
-                                    temp_cate_menu += `
-                                    <label><input type="checkbox" name="taskid" value="${task_items[k].id}"/>${ban_name}</label>`;
+                                    if(task_items[k].done == 0){
+                                        temp_cate_menu += `
+                                        <label><input type="checkbox" name="taskid" value="${task_items[k].id}"/>${ban_name}</label>`;
+                                    }else if(task_items[k].done == 1 && task_items[k].created_at == today){
+                                        temp_cate_menu += `
+                                        <label>✅(완료) ${ban_name}</label>`;
+                                    }
                                 }
                                 temp_cate_menu += `</td></tbody>`;
                         }
