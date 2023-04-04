9aa0c15305c84b8a2e2dfb8822ecb907c5b2f28a
imjoo0 <im_joo0@naver.com>
2023-04-05 08:51:14 +0900

    tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -365,7 +365,7 @@ async function get_consulting_student(done_code) {
             $('#today_consulting_title').append('   ✏️상담 건수: '+data.length);
             if(data.length == 0){
                 $('#consulting_student_list').hide();
-                $('#consultingstudent_pagination').hide
+                $('#consultingstudent_pagination').hide();
             }else{
                 var temp_consulting_contents_box = '';
                 $.each(data, function (index, consulting) {
