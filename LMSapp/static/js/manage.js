5f74d1f0d062a0780953428ce806d6c81330a6bf
imjoo0 <im_joo0@naver.com>
2023-04-06 13:19:44 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -29,21 +29,21 @@ function getBanlist(){
         dataType: 'json',
         data: {},
         success: function (response) {
-            let answer_rate =  function(answer, all) {
-                if(Object.is(answer/all, NaN)) return 0;
-                else return answer/all*100;
-            }
-            let make_semester=function(semester){
-                if (semester == 1){
-                    return 1;
-                }else if(semester == 2){
-                    return 5;
-                }else if(semester == 0){
-                    return 9;
-                }else{
-                    return semester
-                }
-            }
+            // let answer_rate =  function(answer, all) {
+            //     if(Object.is(answer/all, NaN)) return 0;
+            //     else return answer/all*100;
+            // }
+            // let make_semester=function(semester){
+            //     if (semester == 1){
+            //         return 1;
+            //     }else if(semester == 2){
+            //         return 5;
+            //     }else if(semester == 0){
+            //         return 9;
+            //     }else{
+            //         return semester
+            //     }
+            // }
             let temp_ban_option = '<option value=0 selected>반을 선택해주세요</option>';
             let all_ban = response['all_ban']
             for (i = 0; i < all_ban.length; i++) {
