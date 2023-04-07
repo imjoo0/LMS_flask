9838d950b667973f61a98edcb230bb1fae9ef6d6
imjoo0 <im_joo0@naver.com>
2023-04-07 09:00:53 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -454,7 +454,7 @@ function sodata(){
         temp_html += `
         <td class="col-1">${i}위</td>
         <td class="col-2">${name}</td>
-        <td class="col-1">${make_semester(ban_data[key][0].semester)}월 학기</td>
+        <td class="col-1">${make_semester(switch_out_bans[i]['students'][0].semester)}월 학기</td>
         <td class="col-1">${student_num+total_switch_count+total_out_count}</td>
         <td class="col-1">${student_num}</td>
         <td class="col-1">${teacher_name}</td>
