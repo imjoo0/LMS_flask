bf246c58ba7ace37bc013175fa7be1b0aa6df42f
imjoo0 <im_joo0@naver.com>
2023-04-07 13:27:13 +0900

    tt

--- LMSapp/views/manage.py
+++ LMSapp/views/manage.py
@@ -25,7 +25,7 @@ def answer(id):
         target_question.answer = 1
         answer_title = request.form['answer_title']
         answer_contents = request.form['answer_contents']
-        o_ban_id = int request.form['o_ban_id']
+        o_ban_id = request.form['o_ban_id']
         new_answer = Answer(content=answer_contents,title=answer_title,created_at=Today,reject_code=o_ban_id,question_id = id)
         db.session.add(new_answer)
         if target_question.category == 2 and o_ban_id != 0 :    
