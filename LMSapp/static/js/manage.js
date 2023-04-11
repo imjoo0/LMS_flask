fadbbec8ca7f9b781e3bd91aad9cfd2be2a55897
imjoo0 <im_joo0@naver.com>
2023-04-11 17:24:24 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -125,7 +125,7 @@ function so_paginating(done_code) {
         }else{
             $('#so_question').hide()
             $('#so_pagination').hide()
-            let temp_nodatamasg = $(`#question_view option[value="${done_code}"]`).text()+'가 없습니다';
+            let temp_nodatamasg = $(`#question_view option[value="${done_code}"]`).text()+'이 없습니다';
             $('#no_data_msg').html(temp_nodatamasg)
             $('#no_data_msg').show()
         }
