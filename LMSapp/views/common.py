a2c17a87a4a3e019412558f36950d7872532fe67
imjoo0 <im_joo0@naver.com>
2023-04-12 10:28:59 +0900

    tt

--- LMSapp/views/common.py
+++ LMSapp/views/common.py
@@ -43,8 +43,6 @@ def get_ban():
         all_ban = callapi.purple_allinfo('get_all_ban')
         switchstudent = []
         outstudent = []
-        consulting = []
-        task = []
         
         db = pymysql.connect(host='127.0.0.1', user='purple', password='wjdgus00', port=3306, database='LMS',cursorclass=pymysql.cursors.DictCursor)
         try:
@@ -54,18 +52,11 @@ def get_ban():
 
                 cur.execute("SELECT ban_id,teacher_id,student_id,created_at as out_created FROM outstudent;")
                 outstudent = cur.fetchall()
-
-                # cur.execute(f"select consulting.id,consulting.ban_id, consulting.student_id, consulting.done, consultingcategory.id as category_id, consulting.week_code, consultingcategory.name as category, consulting.contents,date_format(consulting.startdate, '%Y-%m-%d') as startdate,date_format(consulting.deadline, '%Y-%m-%d') as deadline, consulting.missed, consulting.created_at, consulting.reason, consulting.solution, consulting.result from consulting left join consultingcategory on consulting.category_id = consultingcategory.id")
-                # consulting = cur.fetchall()
-
-                # cur.execute(f"select task.id, task.category_id, task.contents, task.url, task.attachments, date_format(task.startdate, '%Y-%m-%d') as startdate, date_format(task.deadline, '%Y-%m-%d') as deadline, task.priority, task.cycle, taskcategory.name, taskban.ban_id, taskban.teacher_id, taskban.done from task left join taskcategory on task.category_id = taskcategory.id left join taskban on task.id = taskban.task_id;" )
-                # task = cur.fetchall()
-
         except:
                 print('err')
         finally:
                 db.close()        
-        return jsonify({'all_ban': all_ban,'switchstudent': switchstudent,'outstudent': outstudent,'consulting':consulting,'task':task})
+        return jsonify({'all_ban': all_ban,'switchstudent': switchstudent,'outstudent': outstudent})
 
 @bp.route("/all_students", methods=['GET'])
 def get_all_students():
