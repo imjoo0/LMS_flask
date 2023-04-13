7d4103253538505bcb7d39ffd7dc9da0a68a134f
imjoo0 <im_joo0@naver.com>
2023-04-13 11:27:48 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -109,6 +109,7 @@ function so_paginating(done_code) {
             $('#no_data_msg').hide()
             $('#so_question').show()
             $('#so_pagination').show()
+            container.pagination('destroy');
             container.pagination({
                 dataSource: qdata,
                 prevText: '이전',
@@ -361,6 +362,7 @@ function paginating(done_code) {
             $('#csno_data_msg').hide()
             $('#cs_teacher_question').show()
             $('#pagination').show()
+            container.pagination('destroy');
             container.pagination({
                 dataSource: qdata,
                 prevText: '이전',
@@ -584,6 +586,7 @@ async function uldata() {
     $('#ultitle').empty();
     $('#ul_data_box').show()
     $('#ul_pagination').show()
+    container.pagination('destroy');
     container.pagination({
         dataSource: studentsData,
         prevText: '이전',
