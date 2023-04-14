74534b53862be536efe969683d178422ce386637
imjoo0 <im_joo0@naver.com>
2023-04-14 16:36:23 +0900

    tt

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -1350,7 +1350,7 @@ function get_taskban(key){
             $.each(data, function (index, item) {
                 console.log(item)
                 console.log(item.ban_id)
-                baninfo = banData.filter(b=>b.ban_id == item.ban_id)[0]
+                baninfo = banData.filter(b=>b.id == item.ban_id)[0]
                 console.log(baninfo)
                 item.ban_name =  baninfo.name
                 item.teacher_name =  baninfo.teacher_name
