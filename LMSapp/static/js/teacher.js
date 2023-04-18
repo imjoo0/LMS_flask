7ec3180faff1e4217fc9702a5324b8b263f6ba2c
imjoo0 <im_joo0@naver.com>
2023-04-18 17:59:47 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -411,37 +411,37 @@ async function get_consulting(student_id, is_done) {
     student_report = reportsData.filter(r=>r.student_id == student_id)
     if(student_report.length != 0 ){
         student_report_name = student_report[0].file_name
-        <th class="col-7" onclick="openPopup('../static/uploads/unlearned_standard.pdf')">미학습 기준 확인 ✅</th>
-        $('#pdf-open-btn').addEventListener('click', function(){
-            // PDF 파일 URL
-            var pdfUrl = 'https://www.purpleacademy.co.kr/student/documents_download?file='+student_report[0].enc_name;
+        // <th class="col-7" onclick="openPopup('../static/uploads/unlearned_standard.pdf')">미학습 기준 확인 ✅</th>
+        // $('#pdf-open-btn').addEventListener('click', function(){
+        //     // PDF 파일 URL
+        //     var pdfUrl = 'https://www.purpleacademy.co.kr/student/documents_download?file='+student_report[0].enc_name;
             
-            // PDF.js로 PDF 파일 로드
-            pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc_) {
-                var pdfDoc = pdfDoc_;
-                var currentPage = 1;
-                var pageCount = pdfDoc.numPages;
+        //     // PDF.js로 PDF 파일 로드
+        //     pdfjsLib.getDocument(pdfUrl).promise.then(function(pdfDoc_) {
+        //         var pdfDoc = pdfDoc_;
+        //         var currentPage = 1;
+        //         var pageCount = pdfDoc.numPages;
             
-                // 첫 페이지 로드
-                pdfDoc.getPage(currentPage).then(function(page) {
-                var canvas = document.createElement('canvas');
-                var canvasContext = canvas.getContext('2d');
+        //         // 첫 페이지 로드
+        //         pdfDoc.getPage(currentPage).then(function(page) {
+        //         var canvas = document.createElement('canvas');
+        //         var canvasContext = canvas.getContext('2d');
             
-                var viewport = page.getViewport({ scale: 1.0 });
-                canvas.width = viewport.width;
-                canvas.height = viewport.height;
+        //         var viewport = page.getViewport({ scale: 1.0 });
+        //         canvas.width = viewport.width;
+        //         canvas.height = viewport.height;
             
-                var renderContext = {
-                    canvasContext: canvasContext,
-                    viewport: viewport
-                };
+        //         var renderContext = {
+        //             canvasContext: canvasContext,
+        //             viewport: viewport
+        //         };
             
-                page.render(renderContext).promise.then(function() {
-                    document.getElementById('pdf-container').appendChild(canvas);
-                });
-                });
-            });
-        });
+        //         page.render(renderContext).promise.then(function() {
+        //             document.getElementById('pdf-container').appendChild(canvas);
+        //         });
+        //         });
+        //     });
+        // });
     }
     
 
