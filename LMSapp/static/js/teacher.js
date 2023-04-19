ef1d6a721579605330c5a1b35b84dc89aae743ca
imjoo0 <im_joo0@naver.com>
2023-04-19 17:29:13 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -603,7 +603,7 @@ async function get_consulting(student_id, is_done) {
             }
             let history_reason = target['reason'] == null ? '입력해주세요' : target['reason']
             let history_solution = target['solution'] == null ? '입력해주세요' : target['solution']
-            let history_result = target['result'] == null ? '입력해주세요' : target['result']
+            // let history_result = target['result'] == null ? '입력해주세요' : target['result']
             let temp_consulting_contents_box = `
             <input type="hidden" id="target_consulting_id${i}" value="${consulting_id}" style="display: block;" />
             <p mt-lg-4 mt-5>✅<strong>${category}</strong></br><strong>➖상담 마감일:
@@ -618,11 +618,6 @@ async function get_consulting(student_id, is_done) {
                 <input class="modal-body" type="text" size="50"
                     id="consulting_solution${consulting_id}" placeholder="${history_solution}">
             </div>
-            <div class="modal-body-select-container">
-                <span class="modal-body-select-label">상담 결과</span>
-                <textarea class="modal-body" type="text" rows="5" cols="25"
-                    id="consulting_result${consulting_id}" placeholder="${history_result}"></textarea>
-            </div>
             <p>상담 일시 : ${make_date(history_created)}</p>
             `;
             $('#consulting_write_box').append(temp_consulting_contents_box);
@@ -630,6 +625,11 @@ async function get_consulting(student_id, is_done) {
         let temp_post_box = `<p class="mt-lg-4 mt-5">✔️ 상담 결과 이반 / 취소*환불 / 퇴소 요청이 있었을시 본원 문의 버튼을 통해 승인 요청을 남겨주세요</p>`;
         if(is_done == 0){
             temp_post_box = `
+            <div class="modal-body-select-container">
+                <span class="modal-body-select-label">상담 결과</span>
+                <textarea class="modal-body" type="text" rows="5" cols="25"
+                    id="consulting_result" placeholder="history_result가져와야함"></textarea>
+            </div>
             <div class="modal-body-select-container">
             <span class="modal-body-select-label">부재중</span>
             <label><input type="checkbox" id="missed">부재중</label>
