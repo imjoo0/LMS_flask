ecf5f4f8f996bf3bc35a080c1699b8cb28c145c3
imjoo0 <im_joo0@naver.com>
2023-04-19 13:50:59 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -567,21 +567,22 @@ async function get_consulting(student_id, is_done) {
             return { [v]: items };
         });
     }
-    let temp_student_unlearned_totalreport = `<a class="btn-two green rounded" onclick="get_consulting_history_by_cate(${0})">전체 상담</a>`;
+    let temp_student_unlearned_totalreport = `<a class="btn-two green mini" onclick="get_consulting_history_by_cate(${0})">전체 상담</a>`;
     $.each(consultings, function (index, consulting) {
         category = Object.keys(consulting)[0]
         if(consulting[category][0].category_id > 100){
             temp_student_unlearned_totalreport += `
-            <a class="btn-two yellow rounded" onclick="get_consulting_history_by_cate(${consulting[category][0].category_id})">${category} ${consulting[category].length}건</a>
+            <a class="btn-two blue mini" onclick="get_consulting_history_by_cate(${consulting[category][0].category_id})">${category} ${consulting[category].length}건</a>
+            <a href="#" class="btn-two green mini">Button</a>
             `;
         }else{
             temp_student_unlearned_totalreport += `
-            <a class="btn-two blue rounded" onclick="get_consulting_history_by_cate(${consulting[category][0].category_id})">${category} ${consulting[category].length}건</a>
+            <a class="btn-two yellow mini" onclick="get_consulting_history_by_cate(${consulting[category][0].category_id})">${category} ${consulting[category].length}건</a>
             `;
         }
     });
     temp_student_unlearned_totalreport += `
-            <a class="btn-two red rounded" onclick="get_consulting_history_by_cate(${-1})">기한 지난 상담 ${cant_consulting_list_num}건</a>
+            <a class="btn-two red mini" onclick="get_consulting_history_by_cate(${-1})">기한 지난 상담 ${cant_consulting_list_num}건</a>
             `;
     $('#consulting_contents_box').html(temp_student_unlearned_totalreport)
     
