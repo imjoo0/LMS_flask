dbefeccc26b988e100c8dea8d5ea83d74349ee56
imjoo0 <im_joo0@naver.com>
2023-04-19 13:47:51 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -570,13 +570,13 @@ async function get_consulting(student_id, is_done) {
     let temp_student_unlearned_totalreport = `<a class="btn-two green rounded" onclick="get_consulting_history_by_cate(${0})">전체 상담</a>`;
     $.each(consultings, function (index, consulting) {
         category = Object.keys(consulting)[0]
-        if(consulting[category].category_id > 100){
+        if(consulting[category][0].category_id > 100){
             temp_student_unlearned_totalreport += `
-            <a class="btn-two yellow rounded" onclick="get_consulting_history_by_cate(${consulting[category].category_id})">${category} ${consulting[category].length}건</a>
+            <a class="btn-two yellow rounded" onclick="get_consulting_history_by_cate(${consulting[category][0].category_id})">${category} ${consulting[category].length}건</a>
             `;
         }else{
             temp_student_unlearned_totalreport += `
-            <a class="btn-two blue rounded" onclick="get_consulting_history_by_cate(${consulting[category].category_id})">${category} ${consulting[category].length}건</a>
+            <a class="btn-two blue rounded" onclick="get_consulting_history_by_cate(${consulting[category][0].category_id})">${category} ${consulting[category].length}건</a>
             `;
         }
     });
