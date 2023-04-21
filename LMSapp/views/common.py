5a8b0424335540da205ca6a3c2e35875dab18497
imjoo0 <im_joo0@naver.com>
2023-04-21 17:12:40 +0900

    t

--- LMSapp/views/common.py
+++ LMSapp/views/common.py
@@ -119,7 +119,7 @@ def download_file(q_id):
         with zipfile.ZipFile(zip_buffer, "w") as zip_file:
             for attachment in attachments:
                 file_buffer = BytesIO(attachment.data)
-                zip_file.write(file_buffer.getvalue(), attachment.file_name.replace('\0', 'file'))
+                zip_file.writestr(attachment.file_name.replace('\0', ''), file_buffer.getvalue())
         zip_buffer.seek(0)
         return send_file(zip_buffer, as_attachment=True, mimetype='application/zip', download_name='attachments.zip')
 
