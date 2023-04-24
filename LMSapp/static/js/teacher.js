f5075915ef2939753654af375a3c0fc609341b3b
imjoo0 <im_joo0@naver.com>
2023-04-24 18:13:02 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -43,6 +43,7 @@ function get_data() {
             $('#ban_chart_list').empty()
             let unlearned_t =response['all_consulting'].length > 0 ? response['all_consulting'].filter(consulting => consulting.category_id < 100).length : 0;
             let temp_ban_option = '<option value="none" selected>반을 선택해주세요</option>';
+            console.log(response['ban_data'])
             for (i=0;i< response['ban_data'].length;i++) {
                 let register_no =  response['ban_data'][i]['register_no']
                 let name =  response['ban_data'][i]['name']
