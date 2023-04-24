9076e413affd23e1435d0e4716826aab05ba0e93
imjoo0 <im_joo0@naver.com>
2023-04-24 18:30:05 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -48,7 +48,7 @@ function get_data() {
                 banArray.push(response['ban_data']);
                 response['ban_data'] = banArray;
             }
-            console.log(repsponse['ban_data'])
+            console.log(response['ban_data'])
             for (i=0;i< response['ban_data'].length;i++) {
                 let register_no =  response['ban_data'][i]['register_no']
                 let name =  response['ban_data'][i]['name']
