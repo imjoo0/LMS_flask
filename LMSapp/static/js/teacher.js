ea0ae6c6f4ec13705dc8f8fed5f1310b1bce5a4c
imjoo0 <im_joo0@naver.com>
2023-04-24 11:16:34 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -87,7 +87,7 @@ function get_data() {
                             <table class="table text-center" id="class_list">
                                 <tbody style="width:100%;">
                                     <tr class="row">
-                                        <th class="col-12" data-bs-toggle="modal" data-bs-target="#consulting_history_list" onclick="get_student(${register_no})">${name}반  원생 목록  ✔️</th>
+                                        <th class="col-12" data-bs-toggle="modal" data-bs-target="#ban_student_list" onclick="get_student(${register_no})">${name}반  원생 목록  ✔️</th>
                                     </tr>
                                     <tr class="row">
                                         <th class="col-12">총 미학습 ${unlearned}건  (${answer_rate(unlearned, unlearned_t).toFixed(2)}%)</th>
