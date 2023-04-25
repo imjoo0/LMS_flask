55df05a56fee62763f7160694a878b300bed7ae2
imjoo0 <im_joo0@naver.com>
2023-04-25 10:39:32 +0900

    yy

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -1121,7 +1121,9 @@ async function get_question_detail(q_id) {
     $('#question_pagination').hide()
     $('#questiondetail').show()
     questiondata = questionAnswerdata.filter( q=> q.id == q_id)[0]
-    ban_student_data = allStudentData.filter(s=>s.student_id == questiondata.student_id)[0]
+    ban_student_data = allStudentData.filter(s=>s.student_id == questiondata.student_id)
+    console.log(ban_student_data)
+    console.log(ban_student_data[0])
     let temp_question_list = `
     <div class="modal-body-select-container">
         <span class="modal-body-select-label">문의 종류</span>
