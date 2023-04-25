2cd65a77537373f7d437772b7f9e23e4585cb0eb
imjoo0 <im_joo0@naver.com>
2023-04-25 13:45:56 +0900

    yy

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -245,7 +245,7 @@ function get_data() {
                                             <label><input type="checkbox" name="taskid" value="${task_items[k].id}"/>${ban_name.name}</label>`;
                                         }else if(task_items[k].done == 1){
                                             console.log(typeof(task_items[k].created_at))
-                                            console.log(new Date(task_items[k].created_at).getTime())
+                                            console.log(new Date(task_items[k].created_at).setHours(0,0,0,0))
                                             console.log(today)
                                             if(today){
                                                 temp_cate_menu += `
