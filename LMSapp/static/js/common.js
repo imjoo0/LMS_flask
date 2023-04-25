ea5bdf37347f8fbadbb666adb36b36d06899d7f7
imjoo0 <im_joo0@naver.com>
2023-04-25 17:01:29 +0900

    t

--- LMSapp/static/js/common.js
+++ LMSapp/static/js/common.js
@@ -135,14 +135,14 @@ async function get_all_ban() {
             elem.switch_plus_num = switchstudentData.filter(a => a.switch_ban_id == elem.ban_id).length
         });
 
-        banData = response['all_ban']
-        // .sort((a, b) =>{
-        //     if (b.out_num_per !== a.out_num_per) {
-        //         return b.out_num_per - a.out_num_per; // out_num_per 큰 순으로 정렬
-        //     }else{
-        //         return b.student_num - a.student_num; // students.length가 큰 순으로 정렬
-        //     }
-        // })
+        banData = response['all_ban'].map((item) => item.out_num_per = answer_rate(Number(item.out_student_num),totalOutnum).toFixed(0))
+        banData.sort((a, b) =>{
+            if (b.out_num_per !== a.out_num_per) {
+                return b.out_num_per - a.out_num_per; // out_num_per 큰 순으로 정렬
+            }else{
+                return b.student_num - a.student_num; // students.length가 큰 순으로 정렬
+            }
+        })
     } catch (error) {
         alert('Error occurred while retrieving data.');
     }
