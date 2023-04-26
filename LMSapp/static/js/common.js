00f73ab1943dfac6d2323bbd092602ef15ea965c
imjoo0 <im_joo0@naver.com>
2023-04-26 20:37:38 +0900

    tt

--- LMSapp/static/js/common.js
+++ LMSapp/static/js/common.js
@@ -411,89 +411,6 @@ function semesterShow(semester) {
     });
 
 }
-function displayData(totalData, currentPage, dataPerPage,data_list) {
-    let chartHtml = "";
-
-    //Number로 변환하지 않으면 아래에서 +를 할 경우 스트링 결합이 되어버림.. 
-    currentPage = Number(currentPage);
-    dataPerPage = Number(dataPerPage);
-    let last_item = (currentPage - 1) * dataPerPage + dataPerPage;
-    if( last_item > totalData){
-        last_item = totalData
-    }
-    for (
-        var i = (currentPage - 1) * dataPerPage; //11*5 = 55
-        i < last_item; // 55+5
-        i++
-    ) {
-        chartHtml +=`
-        <td class="col-3">${data_list[i].student_name}( ${data_list[i].student_engname} )</td>
-        <td class="col-2">${data_list[i].origin}</td>
-        <td class="col-3">${data_list[i].pname} ( 📞${data_list[i].pmobileno} )</td>
-        <td class="col-3">${data_list[i].unlearned}건 ( ${data_list[i].up}% ) </td>
-        <td class="col-1" custom-control custom-control-inline custom-checkbox" data-bs-toggle="modal" data-bs-target="#consultinghistory" onclick="get_consulting_history(${data_list[i].student_id})">📝</td>`;
-    } 
-    $("#s_data").html(chartHtml);
-}
-function paging(totalData, dataPerPage, pageCount, currentPage, data_list) {
-    totalPage = Math.ceil(totalData / dataPerPage); //총 페이지 수
-
-    if (totalPage < pageCount) {
-        pageCount = totalPage;
-    }
-
-    let pageGroup = Math.ceil(currentPage / pageCount); // 페이지 그룹 1/10 1~10까지는 '1' , 11~20 까지는 2 , 21~30까지는 3 
-    let last = pageGroup * pageCount; //화면에 보여질 마지막 페이지 번호
-
-    if (last > totalPage) {
-        last = totalPage;
-    }
-    let first = last - (pageCount - 1); //화면에 보여질 첫번째 페이지 번호
-    let next = last + 1;
-    let prev = first - 1;
-
-    let pageHtml = "";
-
-    if (prev > 0) {
-        pageHtml += "<li><a class='cursor-pointer' id='prev'> 이전 </a></li>";
-    }
-
-    //페이징 번호 표시 
-    for (var i = first; i <= last; i++) {
-        if (currentPage == i) {
-            pageHtml +=
-                "<li class='on'><a class='cursor-pointer' id='" + i + "'>" + i + "</a></li>";
-        } else {
-            pageHtml += "<li><a class='cursor-pointer' id='" + i + "'>" + i + "</a></li>";
-        }
-    }
-
-    if (last < totalPage) {
-        pageHtml += "<li><a class='cursor-pointer' id='next' > 다음 </a></li>";
-    }
-
-    $("#pagingul").html(pageHtml);
-    let displayCount = "";
-    displayCount = " 원생 명단 1 - " + totalPage + " 페이지 / " + totalData + "명";
-    $("#displayCount").text(displayCount);
-
-    //페이징 번호 클릭 이벤트 
-    $("#pagingul li a").click(function () {
-        let $id = $(this).attr("id");
-        selectedPage = $(this).text();
-
-        if ($id == "next") selectedPage = next;
-        if ($id == "prev") selectedPage = prev;
-
-        //전역변수에 선택한 페이지 번호를 담는다...
-        globalCurrentPage = selectedPage;
-
-        //페이징 표시 재호출
-        paging(totalData, dataPerPage, pageCount, selectedPage, data_list);
-        //글 목록 표시 재호출
-        displayData(totalData, selectedPage, dataPerPage,data_list);
-    });
-}
 async function getTeacherInfo(t_id){
     let info = banData.filter(t=>t.teacher_id == t_id)
     if (info.length == 0){
@@ -723,6 +640,89 @@ async function getTeacherInfo(t_id){
         displayData(totalData, 1, dataPerPage, data_list);
         paging(totalData, dataPerPage, pageCount, 1, data_list);
     }
+}
+function displayData(totalData, currentPage, dataPerPage,data_list) {
+    let chartHtml = "";
+
+    //Number로 변환하지 않으면 아래에서 +를 할 경우 스트링 결합이 되어버림.. 
+    currentPage = Number(currentPage);
+    dataPerPage = Number(dataPerPage);
+    let last_item = (currentPage - 1) * dataPerPage + dataPerPage;
+    if( last_item > totalData){
+        last_item = totalData
+    }
+    for (
+        var i = (currentPage - 1) * dataPerPage; //11*5 = 55
+        i < last_item; // 55+5
+        i++
+    ) {
+        chartHtml +=`
+        <td class="col-3">${data_list[i].student_name}( ${data_list[i].student_engname} )</td>
+        <td class="col-2">${data_list[i].origin}</td>
+        <td class="col-3">${data_list[i].pname} ( 📞${data_list[i].pmobileno} )</td>
+        <td class="col-3">${data_list[i].unlearned}건 ( ${data_list[i].up}% ) </td>
+        <td class="col-1" custom-control custom-control-inline custom-checkbox" data-bs-toggle="modal" data-bs-target="#consultinghistory" onclick="get_consulting_history(${data_list[i].student_id})">📝</td>`;
+    } 
+    $("#s_data").html(chartHtml);
+}
+function paging(totalData, dataPerPage, pageCount, currentPage, data_list) {
+    totalPage = Math.ceil(totalData / dataPerPage); //총 페이지 수
+
+    if (totalPage < pageCount) {
+        pageCount = totalPage;
+    }
+
+    let pageGroup = Math.ceil(currentPage / pageCount); // 페이지 그룹 1/10 1~10까지는 '1' , 11~20 까지는 2 , 21~30까지는 3 
+    let last = pageGroup * pageCount; //화면에 보여질 마지막 페이지 번호
+
+    if (last > totalPage) {
+        last = totalPage;
+    }
+    let first = last - (pageCount - 1); //화면에 보여질 첫번째 페이지 번호
+    let next = last + 1;
+    let prev = first - 1;
+
+    let pageHtml = "";
+
+    if (prev > 0) {
+        pageHtml += "<li><a class='cursor-pointer' id='prev'> 이전 </a></li>";
+    }
+
+    //페이징 번호 표시 
+    for (var i = first; i <= last; i++) {
+        if (currentPage == i) {
+            pageHtml +=
+                "<li class='on'><a class='cursor-pointer' id='" + i + "'>" + i + "</a></li>";
+        } else {
+            pageHtml += "<li><a class='cursor-pointer' id='" + i + "'>" + i + "</a></li>";
+        }
+    }
+
+    if (last < totalPage) {
+        pageHtml += "<li><a class='cursor-pointer' id='next' > 다음 </a></li>";
+    }
+
+    $("#pagingul").html(pageHtml);
+    let displayCount = "";
+    displayCount = " 원생 명단 1 - " + totalPage + " 페이지 / " + totalData + "명";
+    $("#displayCount").text(displayCount);
+
+    //페이징 번호 클릭 이벤트 
+    $("#pagingul li a").click(function () {
+        let $id = $(this).attr("id");
+        selectedPage = $(this).text();
+
+        if ($id == "next") selectedPage = next;
+        if ($id == "prev") selectedPage = prev;
+
+        //전역변수에 선택한 페이지 번호를 담는다...
+        globalCurrentPage = selectedPage;
+
+        //페이징 표시 재호출
+        paging(totalData, dataPerPage, pageCount, selectedPage, data_list);
+        //글 목록 표시 재호출
+        displayData(totalData, selectedPage, dataPerPage,data_list);
+    });
 }
 function change_studentban_kind(ban_id){
     if(ban_id == "none"){
