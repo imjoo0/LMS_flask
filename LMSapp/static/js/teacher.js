944e35bd906b37f3f961eff210b4b1323aff3eba
imjoo0 <im_joo0@naver.com>
2023-04-26 13:45:55 +0900

    yy

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -45,6 +45,8 @@ async function get_data(){
             temp_ban_option += `<option value=${elem.register_no}>${elem.name} (${semester}월 학기)</option>`;
             let ban_unlearned = UnlearnedConsultingsNum > 0 ? UnlearnedConsultingsData.filter(consulting => consulting.ban_id === elem.register_no) : 0;
             console.log(ban_unlearned)
+            let unlearned_cate = [...new Set(ban_unlearned.map(item => item.category))];
+            console.log(ban_unlearned)
             let switch_minus_num = switchstudentData.length > 0 ? switchstudentData.filter(a => a.ban_id == elem.register_no).length : 0;
             let switch_plus_num = switchstudentData.length > 0 ? switchstudentData.filter(a => a.switch_ban_id == elem.register_no).length : 0;
             let now_student_num = elem.first_student_num - switch_minus_num + switch_plus_num - elem.out_student_num
