d1283a6e9e880ea8f0bca8918d1d7edc4d4f008a
imjoo0 <im_joo0@naver.com>
2023-04-27 10:14:34 +0900

    tt

--- LMSapp/static/js/common.js
+++ LMSapp/static/js/common.js
@@ -506,13 +506,15 @@ async function getTeacherInfo(t_id){
         $('#teachertitle').html(`${info[0].teacher_engname} TEACHER REPORT`)
         $('.mo_inloading').show()
         $('.monot_inloading').hide()
+        let ctx = '';
         if(Chart.getChart('total-chart-element')){
             console.log(Chart.getChart('total-chart-element'))
             Chart.getChart('total-chart-element').destroy()
             $('#chartboxbox').html(`<canvas id="total-chart-element" class="total-chart-element p-sm-3 p-2"></canvas>
             <div class ="chart-data-summary" id="teacher_info_student_num">
                 
-            </div>`)
+            </div>`);
+            ctx = document.getElementById('total-chart-element').getContext('2d');
         }
         if (!consultingData && studentsData && taskData) {
             await get_all_consulting().then(() => {
@@ -635,7 +637,6 @@ async function getTeacherInfo(t_id){
             <span>* 퇴소:${ os }</span>
         `
         $('#teacher_info_student_num').html(temp_teacher_info_student_num)
-        let ctx = document.getElementById('total-chart-element').getContext('2d');
         new Chart(ctx, {
             type: 'doughnut',
             data: {
