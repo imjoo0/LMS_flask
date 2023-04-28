a622addeadfb1c724ccaf94de0dd529472a3906f
imjoo0 <im_joo0@naver.com>
2023-04-28 16:54:48 +0900

    yy

--- LMSapp/static/js/common.js
+++ LMSapp/static/js/common.js
@@ -640,12 +640,13 @@ async function getTeacherInfo(t_id){
         if (chart) {
             chart.destroy();
         }
-        let ctx = document.getElementById('total-chart-element').getContext('2d');
-        const TeacherChart = new Chart(ctx, config);
         let dataset = config.data.datasets;
         dataset[0].data = [total_student_num, ss, hs, os]
+        let ctx = document.getElementById('total-chart-element').getContext('2d');
+        const TeacherChart = new Chart(ctx, config);
+        console.log(config)
         TeacherChart.update();
-        
+
         // 미학습 발생
         $('#ucomcom').html(`<td class="col-6">총 ${unlearned_ttc}건 </td><td class="col-6"><strong> ${answer_rate(unlearned_ttc,TunlearnedData[0].total_unlearned_consulting).toFixed(2)}% </strong></td>`);
         let temp_html = `<th class="col-12"><details>
