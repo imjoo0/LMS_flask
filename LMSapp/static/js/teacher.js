ebf7111a12006c9f25e166371e850975de7e2338
imjoo0 <im_joo0@naver.com>
2023-05-03 14:32:35 +0900

    yy

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -626,10 +626,9 @@ async function student_consulting(student_id) {
     } catch (error){
         alert('Error occurred while retrieving data.');
     }
-    console.log(data['consulting_list'])
-    if(data['consulting_list'].length!=0){
+    if(data['consulting_list']){
         let total_ban_unlearned_consulting = 0
-        $.each(data['consulting_list'], function (index, consulting) {
+        $.each(consultingStudentData, function (index, consulting) {
             total_ban_unlearned_consulting += consulting.consulting_list.filter(u => u.category_id < 100 && u.ban_id == data.ban_id).length
         });
     
