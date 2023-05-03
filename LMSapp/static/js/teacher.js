227cbf85394b69086d8c3bc0b5020919f6caf5d5
imjoo0 <im_joo0@naver.com>
2023-05-03 14:28:35 +0900

    yy

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -598,7 +598,7 @@ async function student_consulting(student_id) {
     $('.mo_inloading').show()
     $('.monot_inloading').hide()
     let container = $('#studentlist_pagination')
-    data = consultingStudentData.filter(e =>e.student_id == student_id)[0]
+    data = Targetdata.filter(e =>e.student_id == student_id)[0]
     $('#ban_student_listModalLabelt').html(`${data['student_name']} 원생 상담일지`)
     $('#studentlist_info_box').html(`
     <th class="col-3">${data.student_name}</th>
