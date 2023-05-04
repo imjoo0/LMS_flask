ad5b48d2d0d514debff49ba935cb8afe3a07aeb9
imjoo0 <im_joo0@naver.com>
2023-05-04 17:59:52 +0900

    yy

--- LMSapp/static/js/common.js
+++ LMSapp/static/js/common.js
@@ -263,28 +263,28 @@ async function get_total_data() {
                             <th class="need">전체</th>
                             <td>${total_student_num}명</td>
                             <td>${total_student_num-totalOutnum}명</td>
-                            <td>${totalOutnum}명(${answer_rate(totalOutnum, total_student_num).toFixed(0)}%)</td>
+                            <td>${totalOutnum}명(${answer_rate(totalOutnum, total_student_num).toFixed(2)}%)</td>
                             <td><span class='cursor-pointer fs-4' onclick="semesterShow(${3}">📜</span></td>
                         </tr>
                         <tr>
                             <th class="need">1월 학기</th>
                             <td>${onesemester_total}명</td>
                             <td>${onesemester_total-oneoutnum}명</td>
-                            <td>${oneoutnum}명(${answer_rate(oneoutnum, onesemester_total).toFixed(0)}%)</td>
+                            <td>${oneoutnum}명(${answer_rate(oneoutnum, onesemester_total).toFixed(2)}%)</td>
                             <td><span class='cursor-pointer fs-4' onclick="semesterShow(${1})">📜</span></td>
                         </tr>
                         <tr>
                             <th class="need">5월 학기</th>
                             <td>${fivesemester_total}명</td>
                             <td>${fivesemester_total-fiveoutnum}명</td>
-                            <td>${fiveoutnum}명(${answer_rate(fiveoutnum, fivesemester_total).toFixed(0)}%)</td>
+                            <td>${fiveoutnum}명(${answer_rate(fiveoutnum, fivesemester_total).toFixed(2)}%)</td>
                             <td><span class='cursor-pointer fs-4' onclick="semesterShow(${2})">📜</span></td>
                         </tr>
                         <tr>
                             <th>9월 학기</th>
                             <td>${ninesemester_total}명</td>
                             <td>${ninesemester_total-nineoutnum}명</td>
-                            <td>${nineoutnum}명(${answer_rate(nineoutnum, ninesemester_total).toFixed(0)}%)</td>
+                            <td>${nineoutnum}명(${answer_rate(nineoutnum, ninesemester_total).toFixed(2)}%)</td>
                             <td><span class='cursor-pointer fs-4' onclick="semesterShow(${0})">📜</span></td>
                         </tr>
                     </table>
