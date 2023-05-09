e78f6ba01bc33b8d249869001a67a3d8a0fc2105
imjoo0 <im_joo0@naver.com>
2023-05-09 09:46:07 +0900

    Tt

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -76,8 +76,8 @@ async function get_data(){
                                     <summary>총 미학습 ${ban_unlearned_num}건  (${answer_rate(ban_unlearned_num, UnlearnedConsultingsNum).toFixed(0)}%)</summary>
                                     <ul>
                                     `;
-            console.log(ban_unlearned)
-            if(ban_unlearned_num != 0 ){
+            
+            if(ban_unlearned != 0 ){
                 let unlearned_cate = [...new Set(ban_unlearned.map(item => item.category))];
                 unlearned_cate.forEach((category) => {
                     let num = ban_unlearned.filter(u=>u.category == category).length
