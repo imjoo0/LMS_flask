6abe30c746232dff9d8ac51c933fd8a4acdfff6c
imjoo0 <im_joo0@naver.com>
2023-05-11 14:39:44 +0900

    전체 상담관리

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -1253,6 +1253,7 @@ async function getChunkedConsultingStudentsData() {
 
     studentConsultingWorker.postMessage('fetchStudentsConsultingData');
   
+    const combinedData = await Promise.all(studentConsultingPromise)
     const chunkedConsultingsData = chunkArray(combinedData, 10);
   
     for (const chunk of chunkedConsultingsData) {
