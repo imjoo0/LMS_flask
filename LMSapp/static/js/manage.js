f23ced8804e59ee2adc76904c06b262ae3033442
imjoo0 <im_joo0@naver.com>
2023-05-12 15:20:37 +0900

    전체 상담관리 데이터 가져오기 기능

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -1257,69 +1257,46 @@ async function get_request_consulting(){
     $('#my_consulting_requestModalLabel').html('요청한 상담 목록');
     $('.mo_inloading').show()
     $('.not_inloading').hide()
-    function renderConsultingsData(data) {
-        // 데이터를 사용자에게 표시하는 로직
-        // var idxHtml = `<option value="none">전체</option>`;
-        var dataHtml = '';
-        $.each(data, function (index, consulting) {
-            const ban_name = banData.filter(b=>b.ban_id == consulting.ban_id)[0].ban_name
-            dataHtml += `
-            <td class="col-2">"${make_date(consulting.startdate)}" ~ "${make_date(consulting.deadline)}"</td>
-            <td class="col-1">${consulting.category}</td>
-            <td class="col-2">${consulting.contents}</td>
-            <td class="col-1">${ban_name}</td>
-            <td class="col-1">${consulting.teacher_name}</td>
-            <td class="col-1">${consulting.teacher_mobileno}</td>
-            <td class="col-1">${consulting.student_name} (${consulting.student_engname})</td>
-            <td class="col-1">${consulting.origin}</td>
-            <td class="col-1">${make_reject_code(consulting.done)}</td>
-            <td class="col-1" onclick="get_consultingban(${consulting.id})"> 🔍 </td>`;
-        });
-        // $('#consulting-option').html(idxHtml);
-        $('#tr-row').html(dataHtml);
-    }
-    let container = $('#consulting-pagination');
-    if (!consultingData) {
-        let consultingWorker = new Worker("../static/js/consultings_worker.js");  
-        consultingWorker.postMessage('fetchConsultingData');
-
-        consultingWorker.onmessage = function(event) {
-            const consultingData = event.data.consulting;
-            const chunkedConsultingData = chunkArray(consultingData, 10);
-            console.log(chunkedConsultingData)
-            const paginationOptions = {
-                dataSource: chunkedConsultingData,
-                prevText: '이전',
-                nextText: '다음',
-                pageSize: 10,
-                callback: function (data, pagination) {
-                    const renderedData = data[0]; // 페이지 사이즈가 1이므로 첫 번째 요소만 필요합니다
-                    renderConsultingsData(renderedData);
-                }
-            };
-            container.pagination(paginationOptions);
-    
-            $('.mo_inloading').hide();
-            $('.not_inloading').show();
-        };
+    try {
+        if (!consultingData) {
+            await getConsultingsData()
+        }
         
-    }else{
+        const container = $('#consulting-pagination');
         const paginationOptions = {
-            dataSource: consultingData,
             prevText: '이전',
             nextText: '다음',
             pageSize: 10,
             callback: function (data, pagination) {
                 const renderedData = data[0]; // 페이지 사이즈가 1이므로 첫 번째 요소만 필요합니다
-                renderConsultingsData(renderedData);
+                var dataHtml = '';
+                $.each(data, function (index, consulting) {
+                    console.log(banData)
+                    const ban_name = banData.filter(b=>b.ban_id == consulting.ban_id)[0].ban_name
+                    dataHtml += `
+                    <td class="col-2">"${make_date(consulting.startdate)}" ~ "${make_date(consulting.deadline)}"</td>
+                    <td class="col-1">${consulting.category}</td>
+                    <td class="col-2">${consulting.contents}</td>
+                    <td class="col-1">${ban_name}</td>
+                    <td class="col-1">${consulting.teacher_name}</td>
+                    <td class="col-1">${consulting.teacher_mobileno}</td>
+                    <td class="col-1">${consulting.student_name} (${consulting.student_engname})</td>
+                    <td class="col-1">${consulting.origin}</td>
+                    <td class="col-1">${make_reject_code(consulting.done)}</td>
+                    <td class="col-1" onclick="get_consultingban(${consulting.id})"> 🔍 </td>`;
+                });
+                // $('#consulting-option').html(idxHtml);
+                $('#tr-row').html(dataHtml);
             }
         };
-
-        container.pagination(paginationOptions);
+        container.pagination(Object.assign(paginationOptions, { 'dataSource': consultingData }))
 
         $('.mo_inloading').hide();
         $('.not_inloading').show();
+    }catch (error) {
+        alert('Error occurred while retrieving data.');
     }
+    
     
 }
     
