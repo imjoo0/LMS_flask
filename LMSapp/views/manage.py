186b08e1a4cf59201c2b949aecabafb7173367ca
imjoo0 <im_joo0@naver.com>
2023-05-12 09:58:04 +0900

    상담 요청 알림톡 중복까지 최종 완성 기능

--- LMSapp/views/manage.py
+++ LMSapp/views/manage.py
@@ -427,7 +427,7 @@ def request_ban_student(b_id,t_id,b_name):
         targets = callapi.purple_allinfo('get_student_simple')
         for target in targets:
             print(target)
-            print(type(target['s_id']))
+            print(target['s_id'])
             new_consulting = Consulting(ban_id=b_id,teacher_id=t_id, category_id=received_consulting_category, student_id=target['s_id'],contents=received_consulting_contents, startdate=received_consulting_startdate, deadline=received_consulting_deadline,done=0,missed='1111-01-01')
         db.session.add(new_consulting)
         db.session.commit()
