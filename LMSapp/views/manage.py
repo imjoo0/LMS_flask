eac1eb2ef5e9f0c1ae808e218572d3e2d79e664f
imjoo0 <im_joo0@naver.com>
2023-05-12 10:31:42 +0900

    상담 요청 알림톡 중복까지 최종 완성 기능

--- LMSapp/views/manage.py
+++ LMSapp/views/manage.py
@@ -429,6 +429,7 @@ def request_ban_student(b_id,t_id,b_name):
         #  상담을 마무리할 마감일 저장
         received_consulting_deadline = request.form['consulting_deadline']
         targets = callapi.purple_allinfo('get_student_simple')
+        print(targets)
         for target in targets:
             print(target)
             new_consulting = Consulting(ban_id=b_id,teacher_id=t_id, category_id=received_consulting_category, student_id=target['s_id'],contents=received_consulting_contents, startdate=received_consulting_startdate, deadline=received_consulting_deadline,done=0,missed='1111-01-01')
