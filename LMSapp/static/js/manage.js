ecae6e6d9cf619bcef5df0721f05269ff2e7b0a9
imjoo0 <im_joo0@naver.com>
2023-05-12 10:28:53 +0900

    상담 요청 알림톡 중복까지 최종 완성 기능

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -1204,7 +1204,7 @@ function post_consulting_request() {
                 console.log(v[2])
                 const promise = $.ajax({
                     type: "POST",
-                    url:`/manage/consulting/ban/${totalstudent_ban_id}/${totalstudent_teacher_id}/${ban_name_value}/`,
+                    url:`/manage/consulting/ban/${totalstudent_ban_id}/${totalstudent_teacher_id}/${decodeURIComponent(ban_name_value.replace(/\+/g, ' '))}/`,
                     // data: JSON.stringify(jsonData), // String -> json 형태로 변환
                     data: {
                         consulting_category: consulting_category,
