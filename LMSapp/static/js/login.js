84fa90f0b163683b56fb8036d340102b7d46ea2b
imjoo0 <im_joo0@naver.com>
2023-05-15 10:21:04 +0900

    로그인 예외처리 수정

--- LMSapp/static/js/login.js
+++ LMSapp/static/js/login.js
@@ -1,3 +1,11 @@
+var csrf_token = "{{ csrf_token() }}";
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrf_token);
+            }
+        }
+    });
 function sign_in() {
     user_id = $('#user_id').val();
     password = $('#user_pw').val();
