01e27f39a39b167f05b502c732e0df9fe065b451
imjoo0 <im_joo0@naver.com>
2023-05-15 10:06:18 +0900

    1차 수정 완료

---
+++ LMSapp/static/js/login.js
@@ -0,0 +1,36 @@
+function sign_in() {
+    user_id = $('#user_id').val();
+    password = $('#user_pw').val();
+    $.ajax({
+        type: 'POST',
+        url: '/login',
+        data: {
+            user_id: user_id,
+            user_pw: password
+        },
+        contentType: 'application/json',
+        success: function (response) {
+            if (response['result'] == 'success') {
+                $.cookie('mytoken', response['token'], { path: '/' });
+                window.location.replace('/main')
+            } else {
+                var child = document.querySelector(".append_st");
+                if (child.hasChildNodes()) {
+                    child.removeChild(child.childNodes[0]);
+                }
+                var creat_sentence = document.createElement('p');
+                var creat_text = document.createTextNode(response['msg']);
+                creat_sentence.appendChild(creat_text);
+                creat_sentence.classList.add('creatst');
+                child.appendChild(creat_sentence);
+            }
+        }
+    });
+}
+
+$('.form-container').keyup('keyup', function (event) {
+    if (event.keyCode === 13) {
+        $('#btn_login').click();
+    }
+
+});
