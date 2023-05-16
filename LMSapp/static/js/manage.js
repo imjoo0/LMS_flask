3756bec5c6e4ab885163ebd5fe9c261029219386
imjoo0 <im_joo0@naver.com>
2023-05-16 16:30:16 +0900

    t

--- LMSapp/static/js/manage.js
+++ LMSapp/static/js/manage.js
@@ -1355,6 +1355,9 @@ async function get_request_consulting(){
     $('#history_cate').html(idxHtml);
     $('#history_cate, #consulting_list_search_input').on('change keyup', updateSearchResult);
 
+    consultingData.sort(function (a, b) {
+        return new Date(b.created_at) - new Date(a.created_at);
+    });
     Consultingcontainer.pagination(Object.assign(ConsultingpaginationOptions, { 'dataSource': consultingData }))
     $('.mo_inloading').hide();
     $('.not_inloading').show();
