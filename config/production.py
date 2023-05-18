f67c52a988ee184d407eaa501412f5ca3a164baf
imjoo0 <im_joo0@naver.com>
2023-05-18 09:42:49 +0900

    cofig 파일 분리

--- config/production.py
+++
@@ -1,9 +0,0 @@
-from config.default import *
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://purple:wjdgus00@127.0.0.1:3306/LMS'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
-ALLOWED_EXTENSIONS = ["jpg", "png", "mov", "mp4", "mpg","pdf"]
-MAX_CONTENT_LENGTH = 1000 * 1024 * 1024  # 1000mb
-
-PORT = 2305
