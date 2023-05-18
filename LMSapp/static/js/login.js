8f148236ecfb969de79add0eccbcc59b0e2f0beb
imjoo0 <im_joo0@naver.com>
2023-05-18 17:06:50 +0900

    tt

--- LMSapp/static/js/login.js
+++ LMSapp/static/js/login.js
@@ -53,7 +53,65 @@ function find_my_id(){
                 result = response['teacher_info']
                 if(result == 'nodata'){
                     $('#my_id_box').hide()
-                    $('#register_request_box').show()
+                    // $('#register_request_box').show()
+                    var con_val = confirm('상담 페이지 계정 정보가 존재하지 않습니다, 본원에서 검색해보시겠습니까?')
+                    if(con_val){
+                        $.ajax({
+                            type: "GET",
+                            url: "/find_purple_user/"+teacher_kor_name+"/"+teacher_eng_name,
+                            success: function (response) {
+                                {
+                                    result = response['teacher_info']
+                                    if(result == 'nodata'){
+                                        $('#my_id_box').hide()
+                                        alert('본원에 등록된 정보가 없습니다_purpleacademy.co.kr 계정 가입을 먼저 진행해주세요')
+                                    }else{
+                                        let temp_result =''
+                                        for(i=0;i<result.length;i++){
+                                            teacher_id = result[i].id
+                                            temp_result += `
+                                                <p>👉 ${i+1}번 후보</p>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 이름</span></div>
+                                                <div class="col-sm-9">
+                                                    <p id="selname${teacher_id}" val=${result[i].name}>${make_nullcate(result[i].name)}</p>
+                                                    <p id="seleng_name${teacher_id}" val=${result[i].eng_name}> (${make_nullcate(result[i].eng_name)})</p>
+                                                </div>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 생년월일</span></div>
+                                                <div class="col-sm-9">
+                                                    <p>${make_nullcate(result[i].birthday)}</p>
+                                                </div>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 퍼플 가입일</span></div>
+                                                <div class="col-sm-9">
+                                                    <p>${make_nullcate(result[i].joining_date)}</p>
+                                                </div>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 아이디</span></div>
+                                                <div class="col-sm-9">
+                                                    <p>${result[i].user_id}</p>
+                                                </div>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 이메일</span></div>
+                                                <div class="col-sm-9">
+                                                    <p>${make_part(result[i].category)}</p>
+                                                </div>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 연락처</span></div>
+                                                <div class="col-sm-9">
+                                                    <p>${result[i].mobileno}</p>
+                                                </div>
+                                                <div class="col-sm-3 mb-sm-0 mb-2"><span>✅ 이메일</span></div>
+                                                <div class="col-sm-9">
+                                                    <p>${result[i].email}</p>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button class="btn btn-success" onclick="select_teacher_info(${teacher_id})">등록 요청</button>
+                                                </div>
+                                            `;
+                                        }
+                                        $('#my_id_box').html(temp_result)
+                                        $('#my_id_box').show()
+                                    }
+                                }
+                            }
+                        });
+                    }
                 }else{
                     let temp_result =''
                     for(i=0;i<result.length;i++){
@@ -94,30 +152,14 @@ $('.enter_key').keyup('keyup', function (event) {
         $('#findidenter').click();
     }
 });
-function regi_my_id(){
-    let new_id = $('#new_id').val()
-    let new_kor_name = $('#new_kor_name').val()
-    let new_eng_name = $('#new_eng_name').val()
-    let new_pw1 = $('#new_pw1').val()
-    let new_pw2 = $('#new_pw2').val()
-    let new_mobileno = $('#new_mobileno').val()
-    let new_email = $('#new_email').val()
-    if(new_id=="" ||new_kor_name==""||new_eng_name=="" ||new_pw1==""||new_pw2=="" ||new_mobileno==""||new_email==""){
-        alert('정보를 모두 입력해주세요')
-        return;
-    }
-    if(new_pw1 != new_pw2){
-        alert('비밀번호가 동일하지 않습니다')
-        return;
-    }
+
+function select_teacher_info(teacher_id){
+    const teacher_kor_name = $(`#selname${teacher_id}`)
+    const teacher_eng_name = $(`#seleng_name${teacher_id}`)
     $.ajax({
         type: "POST",
-        url: "/find_user/"+new_kor_name+"/"+new_eng_name,
+        url: "/find_user/"+teacher_kor_name+"/"+teacher_eng_name,
         data: {
-            new_id : new_id,
-            new_pw : new_pw1,
-            new_mobileno : new_mobileno,
-            new_email : new_email
         },
         success: function (response) {
             {
@@ -154,4 +196,8 @@ function regi_my_id(){
             }
         }
     });
+}
+function regi_my_id(teacher_id){
+    const my_pw = $('#new_pw1').val()
+    
 }
--- LMSapp/views/main_views.py
+++ LMSapp/views/main_views.py
@@ -107,29 +107,23 @@ def find_user(teacher_kor_name,teacher_eng_name):
         else:
             return jsonify({'teacher_info': 'nodata'})
     else:
+        teacher_id = request.form['teacher_id']
+        new_user = User(id=teacher_id,name=teacher_kor_name,eng_name=teacher_eng_name)
+        print(new_user)
+        
+        # return jsonify({'teacher_info': 'success'})
+
+
+@bp.route('/find_purple_user/<string:teacher_kor_name>/<string:teacher_eng_name>', methods=['GET'])
+def find_purple_user(teacher_kor_name,teacher_eng_name):
+    if request.method == 'GET':
         teacher_info = callapi.find_user(teacher_kor_name,teacher_eng_name)
         print(teacher_info)
         if(teacher_info == False):
             return jsonify({'teacher_info': 'nodata'})
         else:
-            if(len(teacher_info) == 1):
-                print(teacher_info[0])
-                new_id = request.form['new_id']
-                new_pw = request.form['new_pw']
-                new_mobileno = request.form['new_mobileno']
-                new_email = request.form['new_email']
-                hashed_password = hashlib.sha256(new_pw.encode('utf-8')).hexdigest()
-
-                if(User.query.filter(User.user_id == new_id).first() is not None):
-                    return jsonify({'teacher_info': 'dup'})
-                else:
-                    # new_user = User()
-                    return jsonify({'teacher_info': 'success'})
-            else:
-                return jsonify({'teacher_info': 'nodata'})
-            
-    # if form.validate_on_submit():
-    #     session['user_id'] = form.data.get('user_id')
-    #     session['user_registerno'] = teacher_info['register_no']
-    #     return redirect('/')  # 성공하면 home.html로
-    # return render_template('login.html', form=form)
+            print(teacher_info)
+            for teacher in teacher_info:
+                if(teacher):
+                    print(teacher)
+            return jsonify({'teacher_info': teacher_info})
