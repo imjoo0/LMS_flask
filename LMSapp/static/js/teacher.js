6f59fb99e427a73dfb88cc26c01e77d7099225cd
imjoo0 <im_joo0@naver.com>
2023-05-19 17:36:22 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -385,10 +385,11 @@ async function get_student(ban_id) {
     }
     Studentcontainer = $('#ban_student_list_pagination')
     Targetdata = consultingStudentData.filter((e) => {
-        return e.ban_id === ban_id;
+        return e.ban_id === ban_id && e.category_id == 1;
     })
     $('#ban_student_listModalLabelt').html(`${Targetdata[0].ban_name}반 원생 목록`);
     Studentcontainer.pagination(Object.assign(StudentpaginationOptions, { 'dataSource': Targetdata }))
+
     $('#student_list_search_input').on('keyup', function () {
         var searchInput = $(this).val().toLowerCase();
         var filteredData = Targetdata.filter(function (d) {
