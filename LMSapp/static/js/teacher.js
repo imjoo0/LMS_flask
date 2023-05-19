d863a20796674bbd15caa35129ed1259b32ce49e
imjoo0 <im_joo0@naver.com>
2023-05-19 17:11:43 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -53,8 +53,8 @@ async function get_data(){
             // let switch_plus_num = switchstudentData.length > 0 ? switchstudentData.filter(a => a.switch_ban_id == elem.register_no).length : 0;
             let first_student = myStudentData.filter(s=>s.ban_id == elem.register_no)
             elem.first_student_num = first_student.length
-            elem.out_student_num = first_student_num != 0 ? first_student.filter(s=>s.categroy_id == 2 || s.categroy_id == 8).length : 0 
-            elem.hold_student_num = first_student_num != 0 ? first_student.filter(s=>s.categroy_id == 3).length : 0 
+            elem.out_student_num = elem.first_student_num != 0 ? first_student.filter(s=>s.categroy_id == 2 || s.categroy_id == 8).length : 0 
+            elem.hold_student_num = elem.first_student_num != 0 ? first_student.filter(s=>s.categroy_id == 3).length : 0 
             elem.now_student_num = elem.first_student_num - elem.out_student_num - elem.hold_student_num
             let ban_unlearned = UnlearnedConsultingsNum > 0 ? UnlearnedConsultingsData.filter(consulting => consulting.ban_id === elem.register_no) : 0;
             let ban_unlearned_num = ban_unlearned != 0 ? ban_unlearned.length : 0;
