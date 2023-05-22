916e7b00222f8956a953288688c2718c134c2893
imjoo0 <im_joo0@naver.com>
2023-05-22 15:57:13 +0900

    t

--- LMSapp/static/js/teacher.js
+++ LMSapp/static/js/teacher.js
@@ -9,6 +9,7 @@
 //     return str;
 // }
 import { getIsFetching, setIsFetching } from '../js/isFetching.js';
+import { sort_consultingoption } from '../js/teacher_function.js';
 
 const socket  = io();
 socket.on('new_consulting', (data) => {
@@ -546,97 +547,7 @@ async function get_consulting_student(consultingStudentData,done_code) {
 
     sort_consultingoption(consulting_targetdata,'deadline_desc')
 }
-function sort_consultingoption(consulting_targetdata,sortBy) {
-    switch (sortBy) {
-        case "ban_desc":
-        $('#ban_name_sort').html('<strong>반 이름순 정렬👇</strong>')
-        $('#student_name_sort').html('원생 이름순 정렬👉')    
-        $('#deadline_sort').html('마감일 정렬👉')    
-        $('#consulting_sort').html('상담 건 정렬👉')        
-        consulting_targetdata.sort(function (a, b) {
-            var nameA = a.ban_name.toUpperCase(); // 대소문자 구분 없이 비교하기 위해 대문자로 변환
-            var nameB = b.ban_name.toUpperCase(); // 대소문자 구분 없이 비교하기 위해 대문자로 변환
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
-            return 0;
-        });
-        break;
-        case "name_desc":
-            $('#ban_name_sort').html('반 이름순 정렬👉')
-            $('#student_name_sort').html('<strong>원생 이름순 정렬👇</strong>')    
-            $('#deadline_sort').html('마감일 정렬👉')    
-            $('#consulting_sort').html('상담 건 정렬👉')        
-        consulting_targetdata.sort(function (a, b) {
-            var nameA = a.student_name.toUpperCase(); // 대소문자 구분 없이 비교하기 위해 대문자로 변환
-            var nameB = b.student_name.toUpperCase(); // 대소문자 구분 없이 비교하기 위해 대문자로 변환
-            if (nameA < nameB) {
-                return -1;
-            }
-            if (nameA > nameB) {
-                return 1;
-            }
-            return 0;
-        });
-        break;
-    
-        case "deadline_desc":
-            $('#ban_name_sort').html('반 이름순 정렬👉')
-            $('#student_name_sort').html('원생 이름순 정렬👉')    
-            $('#deadline_sort').html('<strong>마감일 정렬👇</strong>')    
-            $('#consulting_sort').html('상담 건 정렬👉')        
-        consulting_targetdata.sort(function (a, b) {
-            return new Date(a.deadline) - new Date(b.deadline);
-        });
-        break;
-    
-        case "consulting_desc":
-            $('#ban_name_sort').html('반 이름순 정렬👉')
-            $('#student_name_sort').html('원생 이름순 정렬👉')    
-            $('#deadline_sort').html('마감일 정렬👉')    
-            $('#consulting_sort').html('<strong>상담 건 정렬👇</strong>') 
-        consulting_targetdata.sort(function (a, b) {
-            return b.consulting_num - a.consulting_num;
-        });
-        break;
-    }
 
-    
-    let Consultingcontainer = $('#consultingstudent_pagination')
-    let ConsultingpaginationOptions = {
-        prevText: '이전',
-        nextText: '다음',
-        pageSize: 10,
-        pageClassName: 'float-end',
-        callback: function (data, pagination) {
-            $('#consulting_student_list').show();
-            $('#consultingstudent_pagination').show();
-            var temp_consulting_contents_box = '';
-            $.each(data, function (index, consulting) {
-                // let value = `${consulting.ban_name}_${consulting.student_name}_${consulting.student_mobileno}_${consulting.student_id}`
-                temp_consulting_contents_box += `
-                <td class="col-2">${consulting.ban_name}</td>
-                <td class="col-2">${consulting.student_name}</br>${consulting.student_origin}</td>
-                <td class="col-2">${consulting.student_birthday}</td>
-                <td class="col-2">${consulting.student_mobileno}</td>
-                <td class="col-2">${consulting.deadline}</td>
-                <td class="col-1">${consulting.consulting_num}</td>
-                <td class="col-1" data-bs-toggle="modal" data-bs-target="#consultinghistory" onclick="get_consulting('${consulting.student_id}')"><span class="cursor-pointer">📝</span></td> 
-                `;
-            });
-            $('#today_consulting_box').html(temp_consulting_contents_box);
-            $('#consulting_student_list').show();
-        }
-    };
-    // 데이터 정렬 후 페이지네이션 다시 설정
-    Consultingcontainer.pagination("destroy");
-    Consultingcontainer.pagination(
-      Object.assign(ConsultingpaginationOptions, { dataSource: consulting_targetdata })
-    );
-}
 
 // 상담기록 날짜별
 async function student_consulting(student_id) {    
