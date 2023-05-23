604991db4521c79bda809e3b0970dcec66b138ac
imjoo0 <im_joo0@naver.com>
2023-05-23 09:10:51 +0900

    t

--- LMSapp/static/js/isFetching.js
+++ LMSapp/static/js/isFetching.js
@@ -30,8 +30,6 @@ export async function getData() {
         alert('Error occurred while retrieving data.');
     }
 }
-// 모듈이 로드될 때 fetchData 함수 실행
-await get_data();
 
 export function getBansData() {
     return ban_data;  // 다른 파일에서 해당 값을 불러올 수 있도록 반환하는 함수
